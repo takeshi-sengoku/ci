a5524ec9d6a3a36a4289f3cc17bccf02118f85fe
takeshi sengoku <takeshi_sengoku@precs.co.jp>
2017-02-21 17:44:15 +0900

    初回コミット

---
+++ src/app/calc.php
@@ -0,0 +1,9 @@
+<?php
+
+//array_slice($_SERVER['argv'], 2);
+
+$ret1 = AtA::push(1)->push('+')->push(1)->calc();
+$ret2 = BtB::push(1)->push('+')->push(1)->calc();
+
+var_dump($ret1, $ret2, $ret1 === $ret2);
+
