f79c04a00b50513439e6a87e6bc351b5a02a9537
takeshi sengoku <takeshi_sengoku@precs.co.jp>
2017-02-22 11:23:59 +0900

    test

--- src/app/calc.php
+++ src/app/calc.php
@@ -1,11 +1,16 @@
 <?php
 
-//array_slice($_SERVER['argv'], 2);
+namespace app;
+
+use \sample\ci\classes\{
+	AtA,
+	BtB
+};
 
 $ret1 = AtA::push(1)->push('+')->push(1)->calc();
 $ret2 = BtB::push(1)->push('+')->push(1)->calc();
 
-var_dump($ret1, $ret2, $ret1 === $ret2);
+echo $ret1, $ret2, $ret1 === $ret2;
 
 class psr2 {
     const psr2 = 'PSR-2違反';
@@ -13,7 +18,3 @@ class psr2 {
     function Error () {
 
     }}
-
-こわれたこーど
-
-さらにこわれたこーど
